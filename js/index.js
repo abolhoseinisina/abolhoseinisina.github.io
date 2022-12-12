c77523997ba824dbc6f88213bebfb4d4417713c6
abolhoseinisina <abolhoseini.sina@gmail.com>
2022-12-12 10:36:59 -0600

    Change csv to json function

--- js/index.js
+++ js/index.js
@@ -6,38 +6,18 @@ function createPost(post){
     clone.getElementsByClassName("post-title")[0].innerHTML = "<h3><b>" + post['title'] + "</b></h3><h5>" + post['subtitle'] + ", <span class='w3-opacity'>" + post['date'] + ', ' + post['time'] + "</span></h5>";
     clone.getElementsByClassName("post-content")[0].textContent = post['content'];
     clone.getElementsByClassName("post-comment")[0].href = "mailto:abolhoseini.sina@gmail.com?subject=Blog post: " + post['title'];
-    elem.before(clone);
-    
+    elem.before(clone);   
 }
 
-function csv2JSON(csv){
-    var lines=csv.split("\n");  
-    var result = [];
-    var headers=lines[0].split(",");
-  
-    for(var i=1;i<lines.length;i++){
-  
-        var obj = {};
-        var currentline=lines[i].split(",");
-  
-        for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
-        }
-  
-        result.push(obj);
-    }
-  
-    return result;
-  }
-
 $(document).ready(function() {
-    
     $.ajax({
         type: "GET",
         url: "https://raw.githubusercontent.com/abolhoseinisina/abolhoseinisina.github.io/main/content/posts.csv",
         dataType: "text",
         success: function(posts) {
-            posts = csv2JSON(posts);
+            console.log(posts)
+            posts = $.csv.toObjects(posts);
+            
             for (let i = 0; i < posts.length; i++) {
                 createPost(posts[i]);
             }
