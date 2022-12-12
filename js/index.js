bd3b9e76d059a6c591dd93b1f6487d1ccf2d8373
abolhoseinisina <abolhoseini.sina@gmail.com>
2022-12-12 10:45:00 -0600

    modify showing time of posts

--- js/index.js
+++ js/index.js
@@ -1,9 +1,19 @@
 function createPost(post){
     var elem = document.querySelector('#postTemp');
     var clone = elem.cloneNode(true);
+    
+    const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+    postDatetime = new Date(post['date'] + ' ' + post['time']);
+    date = postDatetime.toLocaleString("default", options);
+
     clone.id = 'post-' + post['postNumber'];
     clone.getElementsByClassName("post-image")[0].src = post['picture'];
-    clone.getElementsByClassName("post-title")[0].innerHTML = "<h3><b>" + post['title'] + "</b></h3><h5>" + post['subtitle'] + ", <span class='w3-opacity'>" + post['date'] + ', ' + post['time'] + "</span></h5>";
+    clone.getElementsByClassName("post-title")[0].innerHTML = "<h3><b>" + post['title'] + "</b></h3><h5>" + post['subtitle'] + 
+                                                              ", <span class='w3-opacity'>" + date + "</span></h5>";
     clone.getElementsByClassName("post-content")[0].textContent = post['content'];
     clone.getElementsByClassName("post-comment")[0].href = "mailto:abolhoseini.sina@gmail.com?subject=Blog post: " + post['title'];
     elem.before(clone);   
